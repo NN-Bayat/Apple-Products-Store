5e2c851a4b535c1792089169782aeec15831077c
NN-Bayat <noorullahbayat1414@gmail.com>
2025-04-12 19:02:02 +0430

    feat: page frame

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="w-full h-screen grid place-items-center">
+      <div className="w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto"></div>
+    </div>
+  );
+};
 
-export default App
+export default App;
